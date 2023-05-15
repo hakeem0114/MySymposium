63a3857f49fe151284005e99c82a096ba02bbda5
hakeem <hakeem0114@gmail.com>
2023-05-15 03:02:56 -0400

    Fixed git ignore

--- server/index.js
+++ server/index.js
@@ -70,7 +70,7 @@ const upload = multer({storage})
 
 
 /***ROUTES WITH FILES****/
-app.post("/auth/register", upload.single("picture"), register)
+// app.post("/auth/register", upload.single("picture"), register)
 
 
 /***MONGOOSE SETUP****/
