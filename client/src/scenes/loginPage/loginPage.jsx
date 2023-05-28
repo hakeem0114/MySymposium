f5a01e50d30504390da56be087aedf5821f09b98
hakeem <hakeem0114@gmail.com>
2023-05-28 18:09:49 -0400

    Adding dist

--- client/src/scenes/loginPage/loginPage.jsx
+++ client/src/scenes/loginPage/loginPage.jsx
@@ -16,7 +16,7 @@ const LoginPage = ()=>{
     //Main Back Image
     //const backImage = '../../../public/backImage.jpg'
     //const backImage = '/public/backImage.jpg'
-    const backImage = '../../../dist/assets/homeback.jpg'
+    const backImage = '../../../public/assets/homeback.jpg'
     
     return <Box
                 sx={{
