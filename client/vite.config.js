9a3e1bf9c943cad6e9a5174d6bb44a146a574cf0
hakeem <hakeem0114@gmail.com>
2023-05-28 00:37:09 -0400

    Adding dist

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MySymposium/client/',
+  base: '/MySymposium/',
   plugins: [react()],
 })
